fee456ae5fe1ea105cbbe052e7575494405e3ba4
ShemmyYo <rabiliamemo@gmail.com>
2023-03-06 10:43:39 +0000

    created view, template and urls

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'email']
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    actions = ['approve_comments', 'undo_approval']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+    def undo_approval(self, request, queryset):
+        queryset.update(approved=False)
